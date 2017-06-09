328a6b780d82e16a7c75d496041c23c9bf541f30
kbrodej <klemen.brodej1@gmail.com>
2017-06-09 21:00:07 +0200

    Finished

---
+++ chatClient.py
@@ -0,0 +1,103 @@
+import socket
+import struct
+import sys
+import threading
+import datetime
+import ssl
+
+#Variables
+
+PORT = 443
+SERVERNAME = 'localhost'
+HEADER_LENGTH = 2
+
+#End of Variables
+
+#Functions
+
+def SetupSslContext(cert, key):
+	context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+	context.verify_mode = ssl.CERT_REQUIRED
+	context.load_cert_chain(certfile = cert, keyfile = key)
+	context.load_verify_locations('streznik.pem')
+	context.set_ciphers('AES128-SHA')
+
+	return  context
+
+def ReceiveFixedLengthMsg(connStream, msgLen):
+	msg = b''
+	while len(msg) < msgLen:
+		chunk = connStream.recv(msgLen - len(msg))
+		if chunk == b'':
+			raise RuntimeError("socket connection broken")
+		msg = msg + chunk
+
+	return msg
+
+def ReceiveMessage(connStream):
+	header = ReceiveFixedLengthMsg(connStream, HEADER_LENGTH)
+	msgLen = struct.unpack("!H", header)[0]
+
+	msg = None
+	if msgLen > 0:
+		msg = ReceiveFixedLengthMsg(connStream, msgLen)
+		msg = msg.decode("utf-8")
+
+	return msg
+
+def SendMsg(connStream, msg):
+	encodedMsg = msg.encode("utf-8")
+
+	header = struct.pack("!H", len(encodedMsg))
+
+	msg = header + encodedMsg
+
+	connStream.sendall(msg);
+
+def messageListener():
+	while True:
+		msgReceived = ReceiveMessage(connStream)
+		if len(msgReceived) > 0:
+			print("[RKchat] " + msgReceived)
+
+def Msg():
+	receiver = input("input receiver ('global' for global chat):")
+	time = "(" + datetime.datetime.now().strftime("%H:%M:%S") + ")"
+	actualMsg = input("message:")
+	packed = time+"<msg><to>"+receiver+"</to><text>"+actualMsg+"</text></msg>"
+
+	return packed
+
+def First():
+	msg = ' '
+	return msg
+
+#End of Functions
+
+print("[system] connecting to chat server ...")
+i = input("input username who has .pem file: ")
+cert = i + ".pem"
+key = i + "key.pem"
+x = SetupSslContext(cert,key) #SSL setup
+
+connStream = x.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_side=False) #SSL wrap
+connStream.connect((SERVERNAME, PORT))
+
+data = connStream.getpeercert()
+sName = data['issuer'][5][0][1]
+
+print("[system] connected to", sName , "on port", PORT)
+
+
+thread = threading.Thread(target=messageListener)
+thread.daemon = True
+thread.start()
+SendMsg(connStream, First())
+
+
+
+while True:
+	try:
+		SendMsg(connStream, Msg())
+	except KeyboardInterrupt:
+		sys.exit()
---
+++ chatServer.py
@@ -0,0 +1,131 @@
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+import socket
+import struct
+import threading
+import ssl
+
+
+#Variables
+
+PORT = 443
+HEADER_LENGTH = 2
+usersonline={}
+
+#End of Variables
+
+#Functions
+
+
+def SetupSslContext():
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_cert_chain(certfile="streznik.pem", keyfile="streznikkey.pem") #Load own certificate
+    context.load_verify_locations('clients.pem')
+    context.set_ciphers('AES128-SHA')
+    return context
+
+
+def ReceiveFixedLengthMsg(connStream, msgLen):
+    try:
+        msg = b''
+        while len(msg) < msgLen:
+            bajti = connStream.recv(msgLen - len(msg))
+            if bajti == b'':
+                raise RuntimeError("socket connection broken")
+            msg = msg + bajti
+        return msg
+    except:
+        connStream.close()
+def ReceiveMessage(connStream):
+    header = ReceiveFixedLengthMsg(connStream, HEADER_LENGTH)
+    msgLen = struct.unpack("!H", header)[0]
+
+    msg = None
+    if msgLen > 0: # ce je vse OK
+        msg = ReceiveFixedLengthMsg(connStream, msgLen)
+        msg = msg.decode("utf-8")
+    return msg
+
+def SendMsg(connStream, msg):
+    encodedMessage = msg.encode("utf-8")
+    header = struct.pack("!H", len(encodedMessage))
+
+    msg = header + encodedMessage
+    connStream.sendall(msg);
+
+#Main changes for SSL
+
+def ClientThread(connStream, certData):
+    global clients
+    sender = certData['issuer'][5][0][1]
+    print("[system] connected with " + sender)
+    print("[system] we now have " + str(len(clients)) + " clients")
+    while True:
+        msg = ReceiveMessage(connStream)
+        if "<msg>" in msg:
+            msg = msg.replace("<msg><to>", " ")
+            msg = msg.replace("</to><text>", " ")
+            msg = msg.replace("</text></msg>", " ")
+            msg2 = msg.split(" ")
+
+            time = msg2[0]
+            receiver  = msg2[1]
+            actualMsg = msg2[2:]
+            joined = "[" + sender + "]" + time + " " + ' '.join(actualMsg)
+            if receiver != "global":
+                if receiver not in usersonline:
+                    SendMsg(usersonline[sender], "User not online")
+                else:
+                    for key in usersonline:
+                        if key == receiver:
+                            SendMsg(usersonline[receiver],joined)
+            else:
+                for client in clients:
+                    SendMsg(client, joined)
+                print("[RKchat]" + joined)
+        else:
+            usersonline[sender] = connStream
+        if not msg:
+            break;
+    with clientsLock:
+        clients.remove(connStream)
+    print("[system] we now have " + str(len(clients)) + " clients")
+    connStream.close()
+
+#End of Functions
+
+#Server start
+
+print("Server is starting!")
+
+mySslCtx = SetupSslContext() #SSL setup
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(("localhost", PORT))
+serverSocket.listen(1)
+
+print("[system] listening ...")
+clients = set()
+clientsLock = threading.Lock()
+
+while True:
+    try:
+        clientSock, clientAddr = serverSocket.accept()
+        try:
+            connStream = mySslCtx.wrap_socket(clientSock, server_side=True) #SSL wrap
+            certData = connStream.getpeercert()
+            with clientsLock:
+                clients.add(connStream)
+            SendMsg(connStream,"Welcome " + certData['issuer'][5][0][1] + " with valid SSL certificate to te RKchat")
+            thread = threading.Thread(target=ClientThread, args=(connStream, certData));
+            thread.daemon = True
+            thread.start()
+        except:
+            connStream.close()
+            clientSock.close()
+    except KeyboardInterrupt:
+        break
+
+print("[system] closing server socket ...")
+serverSocket.close()
